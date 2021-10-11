d592c2be7f2c81d7465e09efd743c1c65d7a1325
obedmacallums <obedmacallums@gmail.com>
2021-10-11 12:09:39 -0300

    added python decouple

--- apiparkingfile/settings.py
+++ apiparkingfile/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$&*^l9rngsnz-508eo*vbcy67xe@qv^a7mw9fe=u#e+ckruh*4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -131,4 +132,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
 
-GOOGLE_MAPS_API_KEY = 'AIzaSyDm0gMbmPbHTaSZDp-0eUN62MhmBuSdQbU'
+GOOGLE_MAPS_API_KEY = config('GOOGLE_MAPS_API_KEY')
