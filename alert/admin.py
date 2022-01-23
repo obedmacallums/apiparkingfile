1cb219edd05edc4dc7e8caeec2c2ad66ca488c9d
obedmacallums <obedmacallums@gmail.com>
2022-01-23 19:34:54 -0300

    registered Alert admin

--- alert/admin.py
+++ alert/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import ConfigAlert
+from .models import ConfigAlert, Alert
 # Register your models here.
 
 class ConfigAlertAdmin(admin.ModelAdmin):
     readonly_fields = ("created", "updated")
 
 admin.site.register(ConfigAlert, ConfigAlertAdmin)
+
+class AlertAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+
+admin.site.register(Alert, AlertAdmin)
